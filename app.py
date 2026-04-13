c2c0c8fa725962e9aeb41908130650f3f3134758
AJAYAKUMAR J <jpgoudaajaykumarj@gmail.com>
2026-04-13 15:28:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import numpy as np
 from PIL import Image
 import json
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['TF_USE_LEGACY_KERAS'] = '1'
-import tensorflow as tf
+
+st.set_page_config(page_title='Crop Disease Detector', page_icon='🌾')
+st.title('🌾 Crop Disease Detector')
+st.write('Upload a Rice or Wheat leaf image to detect disease instantly!')
 
 @st.cache_resource
 def load_model():
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    import tensorflow as tf
     return tf.keras.models.load_model('model.h5', compile=False)
-
-model = load_model()
 
 with open('class_names.json', 'r') as f:
     class_names = json.load(f)
@@ -26,13 +27,10 @@ remedies = {
     'wheat_rust': {'label': '🔴 Wheat — Rust Disease', 'remedy': 'Apply Propiconazole fungicide.'}
 }
 
-st.set_page_config(page_title='Crop Disease Detector', page_icon='🌾')
-st.title('🌾 Crop Disease Detector')
-st.write('Upload a Rice or Wheat leaf image to detect disease instantly!')
-
 uploaded_file = st.file_uploader('📷 Upload Leaf Image', type=['jpg','jpeg','png','webp'])
 
 if uploaded_file is not None:
+    model = load_model()
     image = Image.open(uploaded_file).convert('RGB')
     st.image(image, caption='Uploaded Image', use_column_width=True)
     img = image.resize((224, 224))
